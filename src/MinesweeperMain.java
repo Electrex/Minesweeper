f06d795dd49fbf8075b82a7861decd730eea1908
leoalciso <leoalciso@gmail.com>
2020-12-02 20:54:02 -0800

    Updated Main game modes

--- src/MinesweeperMain.java
+++ src/MinesweeperMain.java
@@ -8,7 +8,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class MinesweeperMain {
     private static final BlockingQueue<Message> queue = new LinkedBlockingQueue<>();
-    static int[] beginner = new int[]{9, 9, 5};
+    static int[] beginner = new int[]{9, 9, 10};
     static int[] intermediate = new int[]{16, 16, 40};
     static int[] expert = new int[]{30, 16, 99};
 
